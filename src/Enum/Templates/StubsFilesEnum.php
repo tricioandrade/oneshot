75d712d6265adc23beabb58ec8aa22a027445dc8
Patrício Andrade <tricioandrade@outlook.com>
2023-09-03 18:34:52 +0100

    :seedling: atubsfiles enum templates requister created

---
+++ src/Enum/Templates/StubsFilesEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OneShot\Builder\Enum\Templates;
+
+enum StubsFilesEnum: string
+{
+    case API_CONTROLLER = 'create.api-controller.stub';
+    case SERVICE        = 'create.service.stub';
+    case TRAITS         = 'create.trait.stub';
+    case ENUM           = 'create.enum.stub';
+
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
